730e99f2549c35ff97d1400400a4dbc99f2026ca
Sergey Sadykov <snsadykov@ya.ru>
2023-02-05 16:46:54 +0300

    ready! thanks!

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,39 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
-
+        int [] count = {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < count.length; i++) {
+//            count [i] = new Random().nextInt(10);
+        }
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
+        for (int i = 0; i < 10; i++) {
+            System.out.println(count[i]);
+        }
+        System.out.println("\n\n");
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
 
+        for (int i = count.length-1; i >=0 ; i--) {
+            System.out.println(count[i]);
+        }
+        System.out.println("\n\n");
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+
+        for (int i = 0; i < count.length /2; i++) {
+            int temp = count [i];
+            count [i] = count[count.length-(1+i)];
+            count [count.length- (1+i)] = temp;
+
+        }
+        for (int i = 0; i < count.length; i++) {
+            System.out.println(count[i]);
+
+        }
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Random;
+
+
+//
+//        Далее вам нужно будет вычислить несколько показателей:
+
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        //Написать программу, которая сгенерирует случайный вес 10 людям в пределах от 40 до 100 кг.
+
+        int[] age = new int[10];
+
+        // задаем диапазон рандома от 40 до 100
+        // (минимальное значение рандома 0, добавив 40 к 0 полуаем нижний порог,
+        // но 40 прибавляется и к 101 и получается 141, поэтому меняем на 101-40=61
+
+        for (int i = 0; i < age.length; i++) {
+            age [i] = new Random().nextInt(61)+40;}
+            for (int i = 0; i < age.length; i++) ;
+
+        //        1. Средний вес всех людей.
+        double sum = 0;
+        for (int i = 0; i < age.length; i++) {
+            sum += age[i];
+            System.out.println("Вес человека: " + age[i]);
+        }
+
+        //        2. Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно
+        int count = 0;
+        for (int i = 0; i < age.length; i++) {
+            if (age [i] >= 60 && age [i] <= 80) {
+            count++;
+        }
+    }
+        System.out.println("Средний вес: "+ sum / age.length);
+        System.out.println("Люди с весом от 60 до 80: "+ count);
+
     }
 }
